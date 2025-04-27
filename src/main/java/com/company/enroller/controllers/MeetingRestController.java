b152e3d5b44ec9182f5b7ea7eb7b10c6b23d3564
machlarz <mimac1982@gmail.com>
2025-04-27 15:21:19 +0200

    Meetings - delete, update

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,9 +1,7 @@
 package com.company.enroller.controllers;
 
 import com.company.enroller.model.Meeting;
-import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
-import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,5 +28,37 @@ public class MeetingRestController {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting) {
+        if (meetingService.findByTitle(meeting.getTitle()) != null) {
+            return new ResponseEntity<String>(
+                    "Unable to create. A meeting with id " + meeting.getId() + " already exist.",
+                    HttpStatus.CONFLICT);
+        }
+        meetingService.add(meeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        meetingService.delete(meeting);
+        return new ResponseEntity<Meeting>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody Meeting updateMeeting) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        meeting.setTitle(updateMeeting.getTitle());
+        meetingService.update(meeting);
+        return new ResponseEntity<Meeting>(HttpStatus.OK);
     }
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,8 +1,8 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
-import com.company.enroller.model.Participant;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -11,21 +11,47 @@ import java.util.Collection;
 @Component("meetingService")
 public class MeetingService {
 
-	Session session;
 	DatabaseConnector connector;
 
 	public MeetingService() {
-		session = DatabaseConnector.getInstance().getSession();
+		connector = DatabaseConnector.getInstance();
 	}
+
 
 	public Collection<Meeting> getAll() {
 		String hql = "FROM Meeting";
-		Query query = this.session.createQuery(hql);
+		Query query = this.connector.getSession().createQuery(hql);
 		return query.list();
 	}
 
 	public Meeting findById(Long id) {
 		return connector.getSession().get(Meeting.class, id);
+	}
+
+	public Meeting findByTitle(String title) {
+		String hql = "FROM Meeting WHERE title = :title";
+		Query query = this.connector.getSession().createQuery(hql);
+		query.setParameter("title", title);
+		return (Meeting) query.uniqueResult();
+	}
+
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
 	}
 
 }
